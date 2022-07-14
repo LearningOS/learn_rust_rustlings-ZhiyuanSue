c26e2fe6a80b1441d53101fba9761cd5cdc620f7
Yi Zhu <zhiyuansue@192.168.0.195>
2022-07-14 23:12:10 +0800

    update

--- exercises/collections/hashmap2.rs
+++ exercises/collections/hashmap2.rs
@@ -23,6 +23,12 @@ enum Fruit {
     Mango,
     Lychee,
     Pineapple,
+    Peach,
+    Pear,
+    Cherry,
+    Kiwi,
+    Lemon,
+    Olive,
 }
 
 fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
@@ -32,12 +38,19 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         Fruit::Mango,
         Fruit::Lychee,
         Fruit::Pineapple,
+        Fruit::Peach,
+        Fruit::Pear,
+        Fruit::Cherry,
+        Fruit::Kiwi,
+        Fruit::Lemon,
+        Fruit::Olive,
     ];
 
     for fruit in fruit_kinds {
         // TODO: Put new fruits if not already present. Note that you
         // are not allowed to put any type of fruit that's already
         // present!
+        basket.entry(fruit).or_insert(100);
     }
 }
 
