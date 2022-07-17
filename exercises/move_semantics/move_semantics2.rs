c7934ebc559cb0939af94f3064bf8442a7365dac
Yi Zhu <zhiyuansue@192.168.0.195>
2022-07-17 21:53:08 +0800

    update

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -6,8 +6,8 @@
 
 fn main() {
     let vec0 = Vec::new();
-
-    let mut vec1 = fill_vec(vec0);
+    let vec00 = vec0.clone();
+    let mut vec1 = fill_vec(vec00);
 
     // Do not change the following line!
     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
